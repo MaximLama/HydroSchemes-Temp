163352eee9c8a42faa8aa4170ac9348950148f02
Ziropirg <56511812+Ziropirg@users.noreply.github.com>
2023-07-16 08:35:22 +0300

    Add files via upload

---
+++ Source/HydroSchemes/Public/HSActors/Hydro_valve.h
@@ -0,0 +1,17 @@
+// HydroSchemes Simulator. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HSActors/SchemeActor.h"
+#include "Hydro_valve.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HYDROSCHEMES_API AHydro_valve : public ASchemeActor
+{
+	GENERATED_BODY()
+	
+};
